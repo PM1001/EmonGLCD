60b7637089f3385d47657f44f6eec6a91de4b8fa
PM1001 <pieterjmarais@gmail.com>
2016-07-13 17:36:02 +0200

    Major changes.

--- HomeEnergyMonitor/HomeEnergyMonitor.ino
+++ HomeEnergyMonitor/HomeEnergyMonitor.ino
@@ -35,102 +35,237 @@
 //
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 
-#define RF69_COMPAT 0 // set to 1 to use RFM69CW 
-#include <JeeLib.h>   // make sure V12 (latest) is used if using RFM69CW
-#include <GLCD_ST7565.h>
-#include <avr/pgmspace.h>
-GLCD_ST7565 glcd;
+#define RF69_COMPAT 1 // set to 1 to use RFM69CW 
+#define RF_ENABLED
+//#define LCD_ON_SPI 1
 
-#include <OneWire.h>		    // http://www.pjrc.com/teensy/td_libs_OneWire.html
-#include <DallasTemperature.h>      // http://download.milesburton.com/Arduino/MaximTemperature/ (3.7.2 Beta needed for Arduino 1.0)
+#ifdef RF_ENABLED
+#include <JeeLib.h>   // make sure V12 (latest) is used if using RFM69CW
+#endif
+#include <U8glib.h>
+#include <avr/pgmspace.h>
+
 
 #include <RTClib.h>                 // Real time clock (RTC) - used for software RTC to reset kWh counters at midnight
 #include <Wire.h>                   // Part of Arduino libraries - needed for RTClib
 RTC_Millis RTC;
 
+// LCD Pins
+//#define U8G_HW_SPI_2X
+U8GLIB_ST7920_128X64_1X glcd(5);    // SCK, SID, CS (hardware based SPI for LCD2)
 //--------------------------------------------------------------------------------------------
 // RFM12B Settings
 //--------------------------------------------------------------------------------------------
 #define MYNODE 20            // Should be unique on network, node ID 30 reserved for base station
-#define RF_freq RF12_433MHZ     // frequency - match to same frequency as RFM12B module (change to 868Mhz or 915Mhz if appropriate)
+#define RF_freq RF12_868MHZ     // frequency - match to same frequency as RFM12B module (change to 868Mhz or 915Mhz if appropriate)
 #define group 210 
 
-#define ONE_WIRE_BUS 5              // temperature sensor connection - hard wired 
+unsigned long fast_update, slow_update=0;
 
-unsigned long fast_update, slow_update;
-
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
 double temp,maxtemp,mintemp;
 
 //---------------------------------------------------
 // Data structures for transfering data between units
 //---------------------------------------------------
-typedef struct { int power1, power2, power3, Vrms; } PayloadTX;         // neat way of packaging data for RF comms
-PayloadTX emontx;
+typedef struct {                //  for data from emonPi for emonGLCD time & "Power Now"
+  byte cmd;
+  byte hour;
+  byte minute;
+  byte second;
+} HeadPi;                    // create JeeLabs RF packet structure
+HeadPi emonPiHead;
+  
+typedef struct {                //  for data from emonPi for emonGLCD time & "Power Now"
+  byte cmd;
+  byte hour;
+  byte minute;
+  byte second;
+  int16_t kWhAccum;
+  int16_t WattsNow;
+  int16_t voltage;
+  int16_t frequency;
+  byte SendToNode;
+} PayloadPi;                    // create JeeLabs RF packet structure
+PayloadPi emonPi;
 
-typedef struct { int temperature; } PayloadGLCD;
+typedef struct {                //  for data from emonPi for emonGLCD time & "Power Now"
+  byte cmd;
+  int16_t outsideTemp;
+  int16_t geyserTemp;
+  int16_t geyserManifold;
+  int16_t pumpReg;
+  byte SendToNode;
+} PayloadPi2;                    // create JeeLabs RF packet structure
+PayloadPi2 emonPi2;
+
+typedef struct { 
+int temper1;
+int temper2;
+int temper3;                                               
+int temper4;                                             
+int temper5;                                           
+int temper6;                                           
+int temper7;                                         
+int temper8; 
+int geyserTemp;
+int collecterTemp;
+int digi1;
+int digi2;
+int digi3; 
+int digi4; 
+} PayloadGeyser;     
+PayloadGeyser emonGeyser;
+typedef struct { int temperature;int digi; } PayloadGLCD;
 PayloadGLCD emonglcd;
 
 int hour = 12, minute = 0;
 double usekwh = 0;
 
 const int greenLED=6;               // Green tri-color LED
-const int redLED=9;                 // Red tri-color LED
+const int redLED=7;                 // Red tri-color LED
+const int LEDpin=9;                 // Red tri-color LED
 const int LDRpin=4;    		    // analog pin of onboard lightsensor 
+const int BacklightPin=3;
 int cval_use;
 
 //-------------------------------------------------------------------------------------------- 
 // Flow control
 //-------------------------------------------------------------------------------------------- 
-unsigned long last_emontx;                   // Used to count time from last emontx update
-unsigned long last_emonbase;                   // Used to count time from last emontx update
+unsigned long last_emonpi;                   // Used to count time from last emontx update
+//--------------------------------------------------------------------------------------------
+// Analog temperature
+//--------------------------------------------------------------------------------------------
+float  convertAnalogToTemperature(unsigned int analogReadValue)
+{
+  // If analogReadValue is 1023, we would otherwise cause a Divide-By-Zero,
+  // Treat as crazy out-of-range temperature.
+  if(analogReadValue == 1023) return 1000.0; 
+                                               
+  return (1/((log(((10000.0 * analogReadValue) / (1023.0 - analogReadValue))/22000.0)/4220.0) + (1 / (273.15 + 25.000)))) - 273.15;
+}
+
+unsigned getTempChannel(byte channel)
+{
+   return (unsigned)(convertAnalogToTemperature(analogRead(channel))*100.0);
+}
+
+void double_LED_flash(byte led)
+{
+  digitalWrite(led, HIGH);  delay(25); digitalWrite(led, LOW);
+  digitalWrite(led, HIGH);  delay(25); digitalWrite(led, LOW);
+}
+
+void single_LED_flash(byte led)
+{
+  digitalWrite(led, HIGH);  delay(100); digitalWrite(led, LOW);
+}
 
 //--------------------------------------------------------------------------------------------
 // Setup
 //--------------------------------------------------------------------------------------------
 void setup()
 {
+    // Set up high frequency PWM for LCD backlight
+  TCCR2A = 0x23; //FAst PWM
+  TCCR2B = 0x09;//62500Hz
+  OCR2A = 255;
+  OCR2B = 30;  // set the PWM 
+  pinMode(BacklightPin, OUTPUT);     // sets as output
+  //digitalWrite(BacklightPin,HIGH);
   delay(500); 				   //wait for power to settle before firing up the RF
-  rf12_initialize(MYNODE, RF_freq,group);
-  delay(100);				   //wait for RF to settle befor turning on display
-  glcd.begin(0x19);
-  glcd.backLight(200);
-  
-  sensors.begin();                         // start up the DS18B20 temp sensor onboard  
-  sensors.requestTemperatures();
-  temp = (sensors.getTempCByIndex(0));     // get inital temperture reading
+
+  glcd.setColorIndex(1);         // pixel on
+  //glcd.setRot180();
+   
+  temp = (getTempChannel(0))/100.0;     // get inital temperture reading
   mintemp = temp; maxtemp = temp;          // reset min and max
+  
+#ifdef RF_ENABLED
+  rf12_initialize(MYNODE, RF_freq,group);
+  delay(100);	//wait for RF to settle befor turning on display
+  int temper = getTempChannel(0);
+  rf12_sendNow(0, &temper, sizeof(int));                     //send temperature data via RFM12B using new rf12_sendNow wrapper -glynhudson
+  rf12_sendWait(2);  
+#endif			
 
   pinMode(greenLED, OUTPUT); 
   pinMode(redLED, OUTPUT); 
+  pinMode(LEDpin, OUTPUT); 
+  digitalWrite(greenLED, HIGH);
+  //single_LED_flash(LEDpin);
+  //single_LED_flash(greenLED);
 }
 
 //--------------------------------------------------------------------------------------------
 // Loop
 //--------------------------------------------------------------------------------------------
+
+void displayUpdate(){
+    cli();
+    glcd.firstPage();           // Update LCD 2
+    //sei();
+    //delay(50);
+    //cli();
+    //delay(50);
+    do {
+      draw_power_page(emonPi.WattsNow, usekwh,temp, mintemp, maxtemp, hour,minute,emonPi2.geyserTemp,emonPi2.outsideTemp,emonPi.voltage,emonPi.frequency,emonPi2.geyserManifold,emonPi2.pumpReg);
+    } while( glcd.nextPage() );
+    sei();
+}
 void loop()
 {
-  
+#ifdef RF_ENABLED
   if (rf12_recvDone())
   {
+    //double_LED_flash(redLED);
     if (rf12_crc == 0 && (rf12_hdr & RF12_HDR_CTL) == 0)  // and no rf errors
     {
-      int node_id = (rf12_hdr & 0x1F);
-      if (node_id == 10) {emontx = *(PayloadTX*) rf12_data; last_emontx = millis();}  //Assuming 10 is the emonTx NodeID
-      
-      if (node_id == 15)			//Assuming 15 is the emonBase node ID
-      {
-        RTC.adjust(DateTime(2012, 1, 1, rf12_data[1], rf12_data[2], rf12_data[3]));
-        last_emonbase = millis();
-      } 
-    }
-  }
 
+      int node_id = (rf12_hdr & 0x1F);
+      if (node_id == 5)			//Assuming 15 is the emonBase node ID
+      {
+         
+        emonPiHead = *(HeadPi*) rf12_data;
+        if(emonPiHead.cmd==0){
+          RTC.adjust(DateTime(2012, 1, 1, emonPi.hour+2, emonPi.minute, emonPi.second));
+          emonPi = *(PayloadPi*) rf12_data;
+        }
+        if(emonPiHead.cmd==1){
+          emonPi2 = *(PayloadPi2*) rf12_data;
+        }
+          
+        last_emonpi = millis();
+      } 
+      if(node_id ==22)
+      {
+        emonGeyser = *(PayloadGeyser*) rf12_data;
+      }
+    }
+    displayUpdate();
+  }
+  #endif
+  if ((millis()-slow_update)>30000)
+  {
+    slow_update = millis();
+    fast_update = millis();
+   
+  rf12_initialize(MYNODE, RF_freq,group);
+  delay(150);	//wait for RF to settle befor turning on display
+  
+#ifdef RF_ENABLED
+    emonglcd.digi = 50;
+    emonglcd.temperature = getTempChannel(0);
+    rf12_sendNow(0, &emonglcd, sizeof(emonglcd));                     //send temperature data via RFM12B using new rf12_sendNow wrapper -glynhudson
+    rf12_sendWait(1);  
+    single_LED_flash(LEDpin);
+    
+#endif
+  }
+  
   //--------------------------------------------------------------------------------------------
-  // Display update every 200ms
+  // Display update every 1000ms
   //--------------------------------------------------------------------------------------------
-  if ((millis()-fast_update)>200)
+  if ((millis()-fast_update)>500)
   {
     fast_update = millis();
     
@@ -139,31 +274,24 @@ void loop()
     hour = now.hour();
     minute = now.minute();
 
-    usekwh += (emontx.power1 * 0.2) / 3600000;
+    usekwh = emonPi.kWhAccum;
     if (last_hour == 23 && hour == 00) usekwh = 0;                //reset Kwh/d counter at midnight
-    cval_use = cval_use + (emontx.power1 - cval_use)*0.50;        //smooth transitions
+    cval_use = emonPi.WattsNow/10.0;//cval_use + (emonPi.WattsNow/10 - cval_use)*0.50;        //smooth transitions
     
-    draw_power_page( "POWER" ,cval_use, "USE", usekwh);
-    draw_temperature_time_footer(temp, mintemp, maxtemp, hour,minute);
-    glcd.refresh();
+    temp = getTempChannel(0)/100.0;
+    if (temp > maxtemp) maxtemp = temp;
+    if (temp < mintemp) mintemp = temp;
+    //rf12_sleep(-1);
 
+    displayUpdate();
     int LDR = analogRead(LDRpin);                     // Read the LDR Value so we can work out the light level in the room.
     int LDRbacklight = map(LDR, 0, 1023, 50, 250);    // Map the data from the LDR from 0-1023 (Max seen 1000) to var GLCDbrightness min/max
     LDRbacklight = constrain(LDRbacklight, 0, 255);   // Constrain the value to make sure its a PWM value 0-255
-    if ((hour > 22) ||  (hour < 5)) glcd.backLight(0); else glcd.backLight(LDRbacklight);  
+    if ((hour > 22) ||  (hour < 5)) OCR2B=50; else OCR2B=LDRbacklight;  
   } 
   
-  if ((millis()-slow_update)>10000)
-  {
-    slow_update = millis();
 
-    sensors.requestTemperatures();
-    temp = (sensors.getTempCByIndex(0));
-    if (temp > maxtemp) maxtemp = temp;
-    if (temp < mintemp) mintemp = temp;
-   
-    emonglcd.temperature = (int) (temp * 100);                          // set emonglcd payload
-    rf12_sendNow(0, &emonglcd, sizeof emonglcd);                     //send temperature data via RFM12B using new rf12_sendNow wrapper -glynhudson
-    rf12_sendWait(2);    
-  }
 }
+
+
+
--- HomeEnergyMonitor/templates.ino
+++ HomeEnergyMonitor/templates.ino
@@ -1,75 +1,101 @@
-#include "utility/font_helvB24.h"
-#include "utility/font_helvB14.h"
-#include "utility/font_helvB12.h"
-#include "utility/font_clR4x6.h"
-#include "utility/font_clR6x8.h"
 
+int qlog10(int num) {
+  if (num >= 1000000)
+    return 6;
+  if (num >= 100000)
+    return 5;
+  if (num >= 10000)
+    return 4;
+  if (num >= 1000)
+    return 3;
+  if (num >= 100)
+    return 2;
+  if (num >= 10)
+    return 1;
+  return 0;
+}
+void  printTime(byte x, byte y,byte hour, byte minute,byte width=6){
+    char chr[2]="0";
+    chr[0] = '0'+(hour/10);
+    glcd.drawStr(x+5, y,chr);
+    chr[0] = '0'+(hour%10);
+    glcd.drawStr(x+width+5, y, chr);
+    glcd.drawStr(x+width*2+4, y-2, ":");
+    chr[0] = '0'+(minute/10);
+    glcd.drawStr(x+width*3-1, y, chr);
+    chr[0] = '0'+(minute%10);
+    glcd.drawStr(x+width*4-1, y, chr);
+  
+}
 //------------------------------------------------------------------
 // Draws a page showing a single power and energy value in big font
 //------------------------------------------------------------------
-void draw_power_page(char* powerstr, double powerval, char* energystr,  double energyval)
+void draw_power_page(double powerval,  double energyval,double temp, double mintemp, double maxtemp, byte hour, byte minute,int16_t geyserTemp,int16_t outsideTemp,int16_t voltage,int16_t frequency,int16_t geyserManifold,int16_t pumpRegister)
 { 
-  glcd.clear();
-  glcd.fillRect(0,0,128,64,0);
+  glcd.setFont(u8g_font_fub17n);
+  glcd.setFontPosTop();  
   
-  char str[50];    			 //variable to store conversion 
-  glcd.setFont(font_clR6x8);      
-  strcpy(str,powerstr);  
-  strcat(str," NOW:"); 
-  glcd.drawString(0,0,str);
-  strcpy(str,energystr);  
-  strcat(str," TODAY:"); 
-  glcd.drawString(0,38,str);
-
-  // power value
-  glcd.setFont(font_helvB24);
-  itoa((int)powerval,str,10);
-  strcat(str,"w");   
-  glcd.drawString(3,9,str);
+  glcd.setPrintPos(4*12-12* qlog10(powerval), 0);
+  glcd.print(powerval, 0); // 0 dp's
+  printTime(128-60,0,hour,minute,12);
   
-  // kwh per day value
-  glcd.setFont(font_clR6x8);
-  if (energyval<10.0) dtostrf(energyval,0,1,str); else itoa((int)energyval,str,10);
-  strcat(str,"kWh");
-  glcd.drawString(87,38,str);        
-}
-
-
-//------------------------------------------------------------------
-// Draws a footer showing GLCD temperature and the time
-//------------------------------------------------------------------
-void draw_temperature_time_footer(double temp, double mintemp, double maxtemp, double hour, double minute)
-{
-  glcd.drawLine(0, 47, 128, 47, WHITE);     //middle horizontal line 
-
-  char str[50];
-  // GLCD Temperature
-  glcd.setFont(font_helvB12);  
-  dtostrf(temp,0,1,str); 
-  strcat(str,"C");
-  glcd.drawString(0,50,str);  
+  glcd.setPrintPos(2*12-8-12* qlog10(energyval/100.0), 18);
+  glcd.print(energyval/100.0, 2); // 0 dp's
   
-  // Minimum and maximum GLCD temperature
-  glcd.setFont(font_clR4x6);             
-  itoa((int)mintemp,str,10);
-  strcat(str,"C");
-  glcd.drawString_P(46,51,PSTR("MIN"));
-  glcd.drawString(62,51,str);
-               
-  itoa((int)maxtemp,str,10); 
-  strcat(str,"C");
-  glcd.drawString_P(46,59,PSTR("MAX"));
-  glcd.drawString(62,59,str);
+    // Write small text to LCD (col, row, text) - Ch1
+  glcd.setFont(u8g_font_6x10);
+  glcd.setFontPosTop();  
+  glcd.drawStr(64, 0, "W");
   
-  // Time
-  char str2[5];
-  itoa((int)hour,str,10);
-  if  (minute<10) strcat(str,": 0"); else strcat(str,": ");
-  itoa((int)minute,str2,10);
-  strcat(str,str2); 
-  glcd.setFont(font_helvB12);
-  glcd.drawString(82,50,str);
+  glcd.drawStr(64, 18, "kWh");
 
+  glcd.setPrintPos(3*6-6* qlog10(voltage/10), 36);
+  glcd.print(voltage/10.0, 1); // 0 dp's
+  glcd.drawStr(36, 36, "V");
+  
+  glcd.setPrintPos(42+2*6-6* qlog10(frequency/10), 36);
+  glcd.print(frequency/10.0, 1); // 0 dp's
+  glcd.drawStr(42+30, 36, "Hz");
+  
+  glcd.drawLine(0,63-17,83,63-17);
+  glcd.drawStr(0, 63-16, "Inside");
+  glcd.setPrintPos(0, 63-8);
+  glcd.print(temp, 1); // 0 dp's
+  char chr[3]=" C";
+  chr[0] = char(176);
+  glcd.drawStr(6*4, 63-8, chr);
+  
+  
+  glcd.drawStr(42, 63-16, "Outsde");
+  glcd.setPrintPos(42, 63-8);
+  glcd.print(outsideTemp/10.0, 1); // 0 dp's
+  glcd.drawStr(42+6*4, 63-8, chr);
+  
+//  glcd.drawStr(128-36, 63-16, "Geyser");
+
+
+  glcd.drawLine(82,19,99,36);
+  glcd.drawLine(85,22,85,62);
+  glcd.drawLine(88,25,88,59);
+  glcd.drawLine(89,59,104,59);
+  glcd.drawLine(85,62,104,62);
+  glcd.drawLine(90,62,104,62);
+  glcd.drawFrame(104, 38, 24, 26);
+  if(RTC.now().second()%2){
+    if((pumpRegister/10)&8)
+      glcd.drawFrame(109, 58, 14, 6);
+    if((pumpRegister/10)&49){     
+      glcd.drawStr(90,50, "P");
+    }
+    
+//        glcd.setPrintPos(90,51);
+//        glcd.print(pumpRegister/10.0, 1); // 0 dp's
+    
+  }
+  glcd.setPrintPos(110, 42);
+  glcd.print(geyserTemp/10, 1); // 0 dp's
+  glcd.setPrintPos(99, 22);
+  glcd.print(geyserManifold/10, 1); // 0 dp's
 }
 
 
